9edf05ab03b4718cbbb3c2c5f35a842fabbc8d83
kader <kader@kader-PC.ad.univ-lyon3.fr>
2015-03-10 11:43:22 +0100

    new commit

--- src/java/Entities/Personne.java
+++ src/java/Entities/Personne.java
@@ -142,6 +142,26 @@ public class Personne implements Serializable {
     }
    
 
+    private String login;
+    private String pswd;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPswd() {
+        return pswd;
+    }
+
+    public void setPswd(String pswd) {
+        this.pswd = pswd;
+    }
+    
+    
     @Override
     public int hashCode() {
         int hash = 0;
